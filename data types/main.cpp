e2207e59792194b87c2ca4c14eff823415bc41c8
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-02 16:50:08 +0300

    Understading variables and data types

--- data types/main.cpp
+++ data types/main.cpp
@@ -14,5 +14,21 @@ int main()
     cout << "Unsigned int can store up to " << sizeof(unsigned int) << " bytes" << endl;
     cout << "Float can store up to " << sizeof(float) << " bytes" << endl;
     cout << "Double can store up to " << sizeof(double) << " bytes" << endl;
+
+    /* Variable declarations */
+    int salary; // variable declaration statement
+    string firstName;
+    double rate;
+
+    salary = 40000; // variable assignment statement
+    rate = .2 * .3;
+    firstName = "Mary";
+
+    int age = 23; // initializating statement
+
+    string middleName, lastName;
+    middleName = "Anne";
+    lastName = "Brown";
+
     return 0;
 }
