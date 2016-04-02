65ba0f5a46586cdd1d4aad6b03d3d656c602ab35
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-02 19:12:17 +0300

    Working with printf()

--- Chap5/main.cpp
+++ Chap5/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <iomanip>
-
+#include <cstdio>
 using namespace std;
 
 int main()
@@ -61,6 +61,46 @@ int main()
     cout << setw(5) << 2 << setw(fieldLength) << mounth2 << endl;
     cout << setw(5) << 3 << setw(fieldLength) << mounth3 << endl;
     cout << setw(5) << 4 << setw(fieldLength) << mounth4 << endl;
+
+
+    /* Working with cstdio library and printf() */
+
+
+    cout << endl << endl;
+
+    double pi = 13.14159265;
+    // printf("%f", pi);
+    // printf("%10f", pi);
+    // printf("%10.3f\n", pi);
+
+    int number = 100;
+    // printf("%d", number);
+    // printf("%15d", number);
+    // Displaying a plus sign
+    // printf("%+15d", number);
+    printf("%s\n", "Hello, world!");
+
+    /*
+
+       We will get an error in this type of situation:
+
+            string hello = "Hello, Somebody";
+            printf("%s", hello);
+
+       It is because of C++ has built-in string data type.
+       But C language - does not. And printf operator
+       is from C language and C library - <cstdio>. So, that's
+       why it does not understand %s. It understands only %c.
+
+    */
+
+
+
+
+    cout << endl << endl;
+
+
+
 
     return 0;
 }
