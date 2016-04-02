9366ecc7ee75e08a6feb7f93cad1b2b6ad06cbae
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-02 16:18:01 +0300

    Finishing some little practical task

---
+++ Hello viewer/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello viewer!" << endl;
+    cout << "I hope you have fun learning C++.";
+    cout << endl;
+    return 0;
+}
