994e4d782ac63aab6df94123742a2d6e72e8fc16
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-03 13:26:33 +0300

    Order of precedence

--- Arithmetic Operators/main.cpp
+++ Arithmetic Operators/main.cpp
@@ -34,6 +34,22 @@ int main()
     result1 = number1 + number2 * 10;
     cout << endl << result1 << endl;
 
+    result1 = (number1 + number2) * 10;
+    cout << result1 << endl;
+
+    result1 = 12 / 2 * 3 + 6 -1;
+    cout << result1 << endl;
+
+    result1 = 12 / 2 * ((3 + 6) -1);
+    cout << result1 << endl;
+
+    int grade1, grade2, grade3;
+    double average;
+    grade1 = 74;
+    grade2 = 82;
+    grade3 = 88;
+    average = (grade1 + grade2 + grade3) / 3;
+    cout << endl << average << endl;
 
 
 
