5c705c178884110cc6431da4cbb135aba1d1cff8
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-03 15:32:09 +0300

    Nested if statement

--- Relational Operators/main.cpp
+++ Relational Operators/main.cpp
@@ -78,6 +78,25 @@ int main()
         Nested if else statement
     */
 
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age < 18) {
+        cout << "You can't vote yet!" << endl;
+        if (age < 16) {
+            cout << "You can't drive yet either." << endl;
+        } else {
+            cout << "You can drive." << endl;
+        }
+    } else {
+        cout << "You can vote." << endl;
+        if (age < 21) {
+            cout << "You can't drink." << endl;
+        } else {
+            cout << "..and you can drink." << endl;
+        }
+    }
+
 
     return 0;
 }
