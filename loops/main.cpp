60f89d6f0999b68de89193410a236b8d536526bf
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-03 17:14:48 +0300

    Intro for while loops:

---
+++ loops/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    /*
+        While loop intro
+
+
+    int count = 1;
+    while (count <= 10) {
+        cout << "Hello World!" << endl;
+        ++count;
+    }
+    return 0;
+
+    */
+
+    /*
+        Two categories of while loops
+
+            Count-Controlled
+            Sentinel-Controlled
+    */
+
+
+}
