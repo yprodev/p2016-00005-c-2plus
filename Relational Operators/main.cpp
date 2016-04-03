21c7ac6d0367d713766b8a257a8396bb03a25018
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-03 15:45:10 +0300

    if else if statements

--- Relational Operators/main.cpp
+++ Relational Operators/main.cpp
@@ -76,7 +76,6 @@ int main()
 
     /*
         Nested if else statement
-    */
 
     int age;
     cout << "Enter your age: ";
@@ -97,6 +96,26 @@ int main()
         }
     }
 
+    */
+
+    int grade;
+    string letterGrade;
+    cout << "Enter a grade: ";
+    cin >> grade;
+    if (grade >= 90) {
+        letterGrade = "A";
+    } else if (grade >= 80) {
+        letterGrade = "B";
+    } else if (grade >= 70) {
+        letterGrade = "C";
+    } else if (grade >= 60) {
+        letterGrade = "D";
+    } else {
+        letterGrade = "F";
+    }
+    cout << "The letter grade for a "
+         << grade << " is a "
+         << letterGrade << endl;
 
     return 0;
 }
