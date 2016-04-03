93146d939c1d8f8007aefc3bd2a12c452368e52a
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-03 19:10:26 +0300

    Loops exercises

--- loops/main.cpp
+++ loops/main.cpp
@@ -188,7 +188,7 @@ int main()
 
     /*
         While loop exercise
-    */
+
 
     string answer = "Watson";
     string response;
@@ -206,9 +206,50 @@ int main()
 
     }
 
+    */
+
+    /*
+        For loop exercise - Factorial
 
 
+    int number, factorial;
+    cout << "Enter a number: ";
+    cin >> number;
+    factorial = 1;
+    for (int i = number; i >= 1; --i) {
+        factorial *= i;
+        cout << factorial << endl;
+    }
 
+    */
+
+    /*
+        For loop - Histogram
+    */
+
+    int data1, data2, data3;
+
+    cout << "Enter the first number: ";
+    cin >> data1;
+    cout << "Enter the second number: ";
+    cin >> data2;
+    cout << "Enter the third number: ";
+    cin >> data3;
+
+    for (int i = 1; i <= data1; ++i) {
+        cout << '*';
+    }
+    cout << data1 << endl;
+
+    for (int i = 1; i <= data2; ++i) {
+        cout << '*';
+    }
+    cout << data2 << endl;
+
+    for (int i = 1; i <= data3; ++i) {
+        cout << '*';
+    }
+    cout << data3 << endl;
 
     return 0;
 
