18a1edd0aad49a5d90ac5b11106805e192d75a00
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-03 13:40:52 +0300

    Using constants

--- Arithmetic Operators/main.cpp
+++ Arithmetic Operators/main.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main()
 {
+
+
     int number1, number2, result1;
     number1 = 22;
     number2 = 2;
+    /*
     result1 = number1 + number2; // Addition - 24
     cout << result1 << endl;
     result1 = number1 - number2; // Subtraction - 20
@@ -23,12 +26,14 @@ int main()
     result2 = number3 - number4;
     result2 = number3 * number4;
     result2 = number3 / number4;
+    */
+
 
     /*
         Using modulus for doubles will give you an error.
     */
     // result2 = number3 % number4;
-    cout << endl << endl;
+    // cout << endl << endl;
 
     /* Order of Precedence */
     result1 = number1 + number2 * 10;
@@ -52,10 +57,34 @@ int main()
     cout << endl << average << endl;
 
 
+    /*
+        Using constants
+    */
 
+    const int numGrade = 3;
+    grade1 = 74;
+    grade2 = 82;
+    grade3 = 88;
+    average = (grade1 + grade2 + grade3) / numGrade;
+    cout << endl << average << endl;
 
+    const double pi = 3.14159265;
+    double circumference;
+    int radius = 3;
+    circumference = 2 * pi * radius;
+    cout << endl << circumference << endl;
 
+    // many lines later
 
+    double area;
+    area = pi * (radius * radius);
+    cout << area << endl;
+
+    // many lines later
+
+    double volume;
+    volume = (4/3) * pi * (radius * radius * radius);
+    cout << volume << endl;
 
 
 
