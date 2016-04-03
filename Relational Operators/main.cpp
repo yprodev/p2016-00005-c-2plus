2deecc63e735e6bc1f5d8363c9d2821446b48819
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-03 15:14:04 +0300

    Simple if statement

--- Relational Operators/main.cpp
+++ Relational Operators/main.cpp
@@ -25,13 +25,34 @@ int main()
 
     /*
         Logical Operators
-    */
 
     int hoursWorked = 40;
     double hourlyRate = 9.75;
     cout << ((hoursWorked < 1) || (hoursWorked > 56)) << endl; // 0 - False
     cout << ((hoursWorked <= 40) && (hourlyRate <= 10.00)) << endl; // 1 - True
     cout << !(hoursWorked == 40) << endl; // 0 - False
+
+    */
+
+    /*
+        Simple IF Statement
+    */
+
+    int hoursWorked;
+    double rate, grossPay;
+    cout << "Enter the hours worked: ";
+    cin >> hoursWorked;
+    cout << "Enter the rate of pay: ";
+    cin >> rate;
+    if (hoursWorked <= 40)
+    {
+        grossPay = hoursWorked * rate;
+    }
+    if (hoursWorked > 40)
+    {
+        grossPay = (40 * rate) + ((hoursWorked - 40) * (rate * 1.5));
+    }
+    cout << "Gross pay is: " << grossPay << endl;
 
 
 
