fb31327126ce09fe9f1061ed8e8526419cc4ea69
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-03 17:37:38 +0300

    Count controlled loop

--- loops/main.cpp
+++ loops/main.cpp
@@ -14,7 +14,6 @@ int main()
         cout << "Hello World!" << endl;
         ++count;
     }
-    return 0;
 
     */
 
@@ -23,7 +22,64 @@ int main()
 
             Count-Controlled
             Sentinel-Controlled
+
+    double balance, rate;
+    int years, count;
+    cout << "What is the starting balance? ";
+    cin >> balance;
+    cout << "What is the annual interest rate? ";
+    cin >> rate;
+    cout << "How many years to calculate interest? ";
+    cin >> years;
+    count = 1;
+    while (count <= years) {
+        balance *= rate;
+        cout << count << ": " << balance << endl;
+        ++count;
+    }
+
+    cout << "After " << years << " years your balance will be " << balance << "." << endl;
+
     */
 
+    /*
+    // Using decrement
+    double balance, rate;
+    int years, count;
+    cout << "What is the starting balance? ";
+    cin >> balance;
+    cout << "What is the annual interest rate? ";
+    cin >> rate;
+    cout << "How many years to calculate interest? ";
+    cin >> years;
+    count = 1;
+    while (years > 0) {
+        balance *= rate;
+        cout << years << ": " << balance << endl;
+        --years;
+    }
+
+    cout << "After " << years << " years your balance will be " << balance << "." << endl;
+
+    */
+
+    int num1, num2, product, count;
+    product = 0;
+    count = 1;
+    cout << "What is the first number? ";
+    cin >> num1;
+    cout << "What is the second number? ";
+    cin >> num2;
+    while (count <= num1) {
+        product += num2;
+        ++count;
+    }
+    cout << num1 << " times " << num2 << " = " << product << endl;
+
+
+
+
+
+    return 0;
 
 }
