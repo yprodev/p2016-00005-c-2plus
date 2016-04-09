bab98f7fc197c10d92bfc3bb5ff14bf5505d54d0
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-09 14:28:51 +0300

    Created conver function for temperature dimentions

--- Function with multiple parameters/main.cpp
+++ Function with multiple parameters/main.cpp
@@ -25,6 +25,13 @@ double ctof(double temp) {
     return (temp * (9.0/5.0) + 32.0);
 }
 
+double convertTemp(double temp, char scale) {
+    if (scale == 'c')
+        return ctof(temp);
+    else if (scale == 'f')
+        return ftoc(temp);
+}
+
 
 int main()
 {
@@ -41,22 +48,33 @@ int main()
     max = maximum(number1, number2, number3);
     cout << "The maximum value is " << max << endl;
 
-    */
+
 
     double fahrTemp, celsius;
-    /*
 
     cout << "Enter a fahrenheit temperature: ";
     cin >> fahrTemp;
     celsius = ftoc(fahrTemp);
     cout << fahrTemp << " fahrenheit is equal to " << celsius << " celsius." << endl;
 
-    */
+
 
     cout << "Enter a celsius temperature: ";
     cin >> celsius;
     fahrTemp = ctof(celsius);
     cout << celsius << " celsius is equal to " << fahrTemp << " fahrenheit." << endl;
+
+    */
+
+    double temp, convertedTemp;
+    char tempScale;
+
+    cout << "Enter a temperature and a scale to convert to: ";
+    cin >> temp >> tempScale;
+
+    convertedTemp = convertTemp(temp, tempScale);
+    cout << "The converted temperature is: "
+         << convertedTemp << endl;
 
     return 0;
 }
