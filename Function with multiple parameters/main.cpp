1f72088ad7ed40477afef57a1ce68ab5a6979976
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-09 14:12:56 +0300

    little more about functions

--- Function with multiple parameters/main.cpp
+++ Function with multiple parameters/main.cpp
@@ -20,9 +20,17 @@ double ftoc(double temp) {
     return (temp - 32.0) * (5.0/9.0);
 }
 
+
+double ctof(double temp) {
+    return (temp * (9.0/5.0) + 32.0);
+}
+
+
 int main()
 {
     //cout << "Hello world!" << endl;
+    /*
+
     int number1, number2, number3, max;
     cout << "Enter the first number: ";
     cin >> number1;
@@ -32,5 +40,23 @@ int main()
     cin >> number3;
     max = maximum(number1, number2, number3);
     cout << "The maximum value is " << max << endl;
+
+    */
+
+    double fahrTemp, celsius;
+    /*
+
+    cout << "Enter a fahrenheit temperature: ";
+    cin >> fahrTemp;
+    celsius = ftoc(fahrTemp);
+    cout << fahrTemp << " fahrenheit is equal to " << celsius << " celsius." << endl;
+
+    */
+
+    cout << "Enter a celsius temperature: ";
+    cin >> celsius;
+    fahrTemp = ctof(celsius);
+    cout << celsius << " celsius is equal to " << fahrTemp << " fahrenheit." << endl;
+
     return 0;
 }
