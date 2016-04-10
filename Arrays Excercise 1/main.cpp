a27b021d917871ee4404b02c48d582e1272ea54b
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-10 15:50:28 +0300

    Creating distribution array

---
+++ Arrays Excercise 1/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void buildArray (int arr[], int size) {
+    srand(time(NULL));
+    for (int i = 0; i < size; ++i) {
+        arr[i] = rand() % 100 + 0;
+    }
+}
+
+void initArray (int arr[], int size) {
+    for (int i = 0; i < size; ++i) {
+        arr[i] = 0;
+    }
+}
+
+void displayArray(int arr[], int size) {
+    for (int i = 0; i < size; ++i) {
+        cout << i << ": " << arr[i] << endl;
+    }
+}
+
+int main()
+{
+
+    const int size = 101;
+    int numbers[size];
+    buildArray(numbers, size);
+    cout << endl;
+    displayArray(numbers, size);
+    cout << endl;
+
+    int distrib[size];
+
+    initArray(distrib, size);
+
+    for (int i = 0; i < size; ++i) {
+        distrib[numbers[i]] += 1;
+    }
+
+    displayArray(distrib, size);
+    cout << endl;
+
+
+    return 0;
+}
