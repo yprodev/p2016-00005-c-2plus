7e37935d81314949df44b8c65d0840099af6fb32
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-23 20:49:16 +0300

    Class constructor functions

--- Classes/main.cpp
+++ Classes/main.cpp
@@ -31,9 +31,56 @@ class Name {
 
 }; // Always put a semi-colon to tell a compiler where the class ends
 
+class Date {
+    private:
+    int month, day, year;
+
+    public:
+    // int month, day, year; - not here, to secure your data
+
+
+    /*
+        Constructor always starts from the name of a class
+        For parameters using just letters is OK.
+
+        We can't use two constructor at the same time, because
+        class constructors can be overloaded. That means we could
+        have two fucntions with the same name but with different
+        number of parameters.
+
+    */
+
+    Date(int m, int d, int y) {
+        month = m;
+        day = d;
+        year = y;
+    }
+
+    // Anther constructor for another format
+    Date(int m, int y) {
+        month = m;
+        day = 0; // We assign here a zero value
+        year = y;
+    }
+
+    // Default constructor
+    Date() {
+        // Default construcor defines just default values for each data variavle
+        month = 0;
+        day = 0;
+        year = 0;
+    }
+};
+
 int main()
 {
     Name myName("Dan", "Mathew", "O'Salliwan");
     cout << myName.toString() << endl;
+
+    Date d1;
+
+    Date today(4,21,2014);
+    Date yesterday(4,2014);
+    Date tomorrow;
     return 0;
 }
