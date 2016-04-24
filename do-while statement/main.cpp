bec31585bc382a3b2ccd84cb4ce2dc9e04e6c411
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-24 21:00:42 +0300

    Rewrite while loop into do-while statement

--- do-while statement/main.cpp
+++ do-while statement/main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main()
 {
-    /* Do-while statement */
+    /*
+
+        Do-while statement
 
     int sum = 0;
     int num = 1;
@@ -16,9 +18,42 @@ int main()
 
     cout << sum << endl;
 
-    /*
-
     */
+
+
+
+    /*
+        Rewriting while loop into do-while statement
+    */
+
+    double balance, rate;
+    int years = 0;
+    balance = 5000;
+
+    cout << "What is the interest rate? ";
+    cin >> rate;
+    while(balance < 10000) {
+        balance *= rate;
+        ++years;
+    }
+
+    cout << "It will take " << years << " years to reach $10,000.00" << endl;
+
+
+    balance = 5000;
+    rate = 0;
+    years = 0;
+
+    cout << "What is the interest rate? ";
+    cin >> rate;
+
+    do {
+        balance *= rate;
+        ++years;
+    } while(balance < 10000);
+
+    cout << "It will take " << years << " years to reach $10,000.00" << endl;
+
 
 
     return 0;
