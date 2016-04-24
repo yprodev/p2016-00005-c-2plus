f46fd5a1956b0884edae98970a5f17c5417f3fc8
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-04-24 20:37:39 +0300

    Using switch statement in a function

--- Switch statement/main.cpp
+++ Switch statement/main.cpp
@@ -2,6 +2,25 @@
 
 using namespace std;
 
+bool isVowel(char letter) {
+    switch(letter) {
+        case 'a':
+        case 'A':
+        case 'e':
+        case 'E':
+        case 'i':
+        case 'I':
+        case 'o':
+        case 'O':
+        case 'u':
+        case 'U':
+            return true;
+            break;
+        default:
+            return false;
+    }
+}
+
 int main()
 {
     char letterGrade;
@@ -32,6 +51,8 @@ int main()
 
     */
 
+    /*
+
     switch(letterGrade) {
         case 'A':
         case 'B':
@@ -46,6 +67,19 @@ int main()
             cout << "Don't recognize an input.";
     }
 
+    */
+
+    string sentence;
+    char ltr;
+    int numVowels = 0;
+    cout << "Enter a sentence: ";
+    getline(cin, sentence);
+    for (int i = 0; i < sentence.size(); ++i) {
+        ltr = sentence[i];
+        if(isVowel(ltr))
+            ++numVowels;
+    }
+    cout << endl << "There are " << numVowels << " vowels in the sentence." << endl;
 
     return 0;
 }
